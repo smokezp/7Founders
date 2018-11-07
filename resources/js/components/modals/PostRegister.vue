5bc479458543144da5890bf5e280d5542047b313
smokezp <smoke111@email.ru>
2018-11-07 14:31:09 +0200

    modals wip

--- resources/js/components/modals/PostRegister.vue
+++ resources/js/components/modals/PostRegister.vue
@@ -15,39 +15,163 @@
                     <div class="modal-body">
                         <slot name="body">
                             <div class="row justify-content-center">
-                                <div class="card w-100">
+                                <div class="w-100">
                                     <div class="card-body">
-                                        Hi, I´m Constantin!
+                                        <div class="col-md-10 m-auto">
+                                            <div class="step-1" v-if="step === 1">
+                                                <div class="text">
+                                                    Hi, I´m Constantin!<br><br>
+                                                    I am the CEO of 7founders and I will help you
+                                                    <br> to setup your expert profile.
+                                                    <br><br>It will only take a minute….
+                                                </div>
+                                            </div>
+                                            <div class="step-2" v-if="step === 2">
+                                                <div class="text">
+                                                    What kind of investment project<br> interest you most?
+                                                </div>
+                                                <div class="projects">
+                                                    <input type="radio"
+                                                           name="name"
+                                                           id="project1"
+                                                           value="mostly_physcial"
+                                                           v-model="investmentProject"
+                                                    />
+                                                    <label for="project1">
+                                                        <div class="box">
+                                                            <div class="userBox"></div>
+                                                            <h3>Something <br> mostly physcial?
+                                                            </h3>
+                                                        </div>
+                                                    </label>
 
-                                        I am the CEO of 7founders and I will help you to setup your expert profile.
+                                                    <input
+                                                        type="radio"
+                                                        name="name"
+                                                        id="project2"
+                                                        value="crypto_blockchain"
+                                                        v-model="investmentProject"
+                                                    />
+                                                    <label for="project2">
+                                                        <div class="box">
+                                                            <div class="userBox"></div>
+                                                            <h3>Something <br> Crypto / Blockchain?
+                                                            </h3>
+                                                        </div>
+                                                    </label>
 
-                                        It will only take a minute….
-                                        <!--<div class="form-group row">-->
-                                            <!--<label for="email"-->
-                                                   <!--class="col-sm-4 col-form-label text-md-right">Email</label>-->
-                                            <!--<div class="col-md-6">-->
-                                                <!--<input v-model="email" id="email" type="email" class="form-control">-->
+                                                    <input
+                                                        type="radio"
+                                                        name="name"
+                                                        id="project3"
+                                                        value="mostly_digital"
+                                                        v-model="investmentProject"
+                                                    />
+                                                    <label for="project3">
+                                                        <div class="box">
+                                                            <div class="userBox"></div>
+                                                            <h3>Something <br> mostly digital?
+                                                            </h3>
+                                                        </div>
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="step-3" v-if="step === 3">
+                                                <div class="text">
+                                                    Do you have additional quick-preferences?
+                                                </div>
+                                                <div class="col-md-8 m-auto">
+                                                    <label class="modal-label">
+                                                        What status range should potential projects have?
+                                                    </label>
 
-                                            <!--</div>-->
-                                        <!--</div>-->
+                                                    <vue-slider v-model="vuSliderSettings.value"
+                                                                :width="vuSliderSettings.width"
+                                                                :height="vuSliderSettings.height"
+                                                                :process-style="vuSliderSettings.processStyle"
+                                                                :tooltip="vuSliderSettings.tooltip"
+                                                                :dot-size="vuSliderSettings.dotSize"
+                                                    ></vue-slider>
+                                                    <div class="row slider-description">
+                                                        <div class="col-md text-left">
+                                                            EARLY <br>SKETCH-OUT
+                                                        </div>
+                                                        <div class="col-md text-center">
+                                                            FULLY OPERATIONAL <br>BUT NOT LIVE
+                                                        </div>
+                                                        <div class="col-md text-right">
+                                                            LIVE & GENERATION <br>REVENUES
+                                                        </div>
+                                                    </div>
+                                                    <label class="modal-label mt-3">
+                                                        SC Investment Forms
+                                                    </label>
+                                                    <div class="row mt-3">
+                                                        <div class="col-md investment-form">
+                                                            MACRO TASKS
+                                                            <switches
+                                                                class="float-right"
+                                                                :theme=vueSwitchSettings.theme
+                                                                v-model="marcoTask"
+                                                                :type-bold="vueSwitchSettings.typeBold"
+                                                            ></switches>
+                                                        </div>
+                                                        <div class="col-md investment-form">
+                                                            CD FOUNDER
+                                                            <switches
+                                                                class="float-right"
+                                                                v-model="cdFounder"
+                                                                :theme=vueSwitchSettings.theme
+                                                                :type-bold="vueSwitchSettings.typeBold"
+                                                            ></switches>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-                                        <!--<div class="form-group row">-->
-                                            <!--<label for="password"-->
-                                                   <!--class="col-md-4 col-form-label text-md-right">Password</label>-->
+                                            </div>
+                                            <div class="step-4" v-if="step === 4">
+                                                <div class="text">
+                                                    Please, tell us a little more about the company
+                                                    <br>you work for and your job.
+                                                </div>
+                                                <label class="modal-label">The company you work for currently</label>
+                                                <div class="row">
+                                                    <div class="col-md">
+                                                        <input type="text" v-model="companyName"
+                                                               placeholder="COMPANY NAME?"
+                                                               class="modal-input"/>
+                                                    </div>
+                                                    <div class="col-md">
+                                                        <input type="text" v-model="activeIndustry"
+                                                               placeholder="ACTIVE IN WITCH INDUSTRY?"
+                                                               class="modal-input"/>
+                                                    </div>
+                                                </div>
+                                                <label class="modal-label">Your job</label>
+                                                <div class="row">
+                                                    <div class="col-md">
+                                                        <input type="text" v-model="jobTitle"
+                                                               placeholder="JOBTITLE"
+                                                               class="modal-input"/>
+                                                    </div>
+                                                    <div class="col-md">
+                                                        <input type="text" v-model="workedHere"
+                                                               placeholder="WORKED HERE HOW LONG?"
+                                                               class="modal-input"/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="step-5" v-if="step === 5">
 
-                                            <!--<div class="col-md-6">-->
-                                                <!--<input id="password" type="password" class="form-control"-->
-                                                       <!--v-model="password">-->
-                                            <!--</div>-->
-                                        <!--</div>-->
+                                            </div>
+                                            <div class="step-6" v-if="step === 6">
 
-                                        <!--<div class="form-group row mb-0">-->
-                                            <!--<div class="col-md-8 offset-md-4">-->
-                                                <!--<button type="submit" class="btn btn-primary" @click="submit">-->
-                                                    <!--Login-->
-                                                <!--</button>-->
-                                            <!--</div>-->
-                                        <!--</div>-->
+                                            </div>
+                                            <button type="button" class="modal-next-btn" @click="nextStep">
+                                                {{step === 1 ? "Lets' Go!" : "Next"}}
+                                                <img class="arrow-right" src="images/arrow-right.png">
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -61,25 +185,83 @@
 
 <script>
     import ClickOutside from 'vue-click-outside';
+    import vueSlider from 'vue-slider-component';
+    import Switches from 'vue-switches';
+    import axios from 'axios';
 
     export default {
         data: () => {
             return {
-                email: null,
-                password: null,
-                hide: false
+                step: 1,
+                investmentProject: null,
+                jobTitle: null,
+                companyName: null,
+                activeIndustry: null,
+                workedHere: null,
+                marcoTask: true,
+                cdFounder: true,
+                last_step: 4,
+                hide: false,
+                value: [0, 30],
+
+                vueSwitchSettings: {
+                    theme: 'custom',
+                    typeBold: true
+                },
+                vuSliderSettings: {
+                    value: [0, 30],
+                    height: 15,
+                    width: '100%',
+                    processStyle: {
+                        backgroundImage: "-webkit-linear-gradient(left, #2dd6e0, #0590dd)"
+                    },
+                    tooltip: false,
+                    dotSize: 32
+                }
             }
         },
         methods: {
             close() {
+                console.log('close');
                 this.hide = true;
             },
-            submit() {
+            nextStep() {
+                switch (this.step) {
+                    case 2:
+                        // axios.post('')
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
 
+                }
+                this.step += 1;
+
+
+                if (this.step > this.last_step) {
+                    console.log('close111');
+                    this.close();
+                }
             }
+        },
+        components: {
+            vueSlider, Switches
         },
         directives: {
             ClickOutside
         }
     }
 </script>
+
+<style scoped>
+
+    .slider-description {
+        font-size: 12px;
+        color: #b5c5cfd6;
+    }
+
+
+</style>
+
+
