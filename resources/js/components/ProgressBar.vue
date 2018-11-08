5c67b11b1d934f22e51c5cbdc33da128551d74c6
smokezp <smoke.31051994>
2018-11-08 21:17:38 +0200

    progress bar wip

--- resources/js/components/ProgressBar.vue
+++ resources/js/components/ProgressBar.vue
@@ -1,11 +1,21 @@
 <template>
-  
+    <div class="progress-container">
+        <div class="progress" style="height: 25px;">
+            <div class="progress-bar" role="progressbar" style="width: {{value}}%"
+                 aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "ProgressBar"
-  }
+    export default {
+        name: "ProgressBar",
+        props: [
+            'height',
+            'width',
+            'value'
+        ]
+    }
 </script>
 
 <style scoped>
